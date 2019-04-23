0c9d31a7b5e25e79a5e415922f9a7003914b9988
Ron.Coleman <ronncoleman@gmail.com>
2019-04-23 15:13:50 -0400

    Added copyright notices to all files and refined the pattern.

--- src/hs/HighScores.java
+++ src/hs/HighScores.java
@@ -1,3 +1,25 @@
+/*
+ Copyright 2014 Ron Coleman
+
+ Permission is hereby granted, free of charge, to any person obtaining
+ a copy of this software and associated documentation files (the
+ "Software"), to deal in the Software without restriction, including
+ without limitation the rights to use, copy, modify, merge, publish,
+ distribute, sublicense, and/or sell copies of the Software, and to
+ permit persons to whom the Software is furnished to do so, subject to
+ the following conditions:
+
+ The above copyright notice and this permission notice shall be
+ included in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package hs;
 
 import java.io.BufferedReader;
@@ -9,15 +31,30 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * This class implements the high scord facade.
+ * @author Ron.Coleman
+ *
+ */
 public class HighScores {
+	// Path in file system where high scores persist
 	public String path = null;
 	
-	private ArrayList<Record> records = new ArrayList<>();
+	// High scores kept in memory in this list
+	protected ArrayList<Record> records = new ArrayList<>();
 	
+	/**
+	 * Constructor
+	 * @param path Path to high scores file
+	 */
 	public HighScores(String path) {
 		this.path = path;
 	}
 	
+	/**
+	 * Loads in the high scores.
+	 * @return List of scores as records of name-score pairs per record
+	 */
 	public ArrayList<Record> load() {
 		records.clear();
 		
@@ -28,6 +65,7 @@ public class HighScores {
 			
 			String line = reader.readLine();
 			
+			// Load in the two-field records -- all others are skipped.
 			while(line != null) {				
 				String[] fields = line.split(" +");
 				
@@ -47,6 +85,9 @@ public class HighScores {
 		catch(FileNotFoundException e) {
 			
 		}
+		catch(NumberFormatException e) {
+			e.printStackTrace();
+		}
 		catch (IOException e) {
 			e.printStackTrace();
 			records = null;
@@ -55,14 +96,23 @@ public class HighScores {
 		return records;
 	}
 	
+	/**
+	 * Gets the records as a list.
+	 * @return List of records
+	 */
 	public ArrayList<Record> getRecords() {
 		return records;
 	}
 	
-	public void save() {
+	/**
+	 * Saves the records sorted in descending order -- whatever is in records.
+	 */
+	public int save() {
 		Collections.sort(records);
 		
 		BufferedWriter writer;
+		
+		int count = 0;
 		
 		try {
 			writer = new BufferedWriter(new FileWriter(path));
@@ -70,6 +120,7 @@ public class HighScores {
 			for(Record record: records) {
 				writer.write(record.getName()+" "+record.getScore());
 				writer.newLine();
+				count++;
 			}
 			
 			writer.flush();
@@ -77,7 +128,9 @@ public class HighScores {
 			writer.close();
 		} catch (IOException e) {
 			e.printStackTrace();
-		}		
+		}	
+		
+		return count;
 	}
 
 	public void clear() {
